1fc795e66b6d95ed9e7297d59952b2f100da1af7
Michoricardo <44788583+michoricardo@users.noreply.github.com>
2024-11-23 19:36:22 -0600

    Agregar todo que se comunica con el puente
    Se agrega funcion para que se agregen todos a la base de datos por medio de express, pero desde el botón de agregar todo que esta en la app móvil

---
+++ Unidad3Actividad2_PrograDispositivosMoviles/App.js
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { Button, View, StyleSheet, TextInput, Alert } from 'react-native';
+
+const App = () => {
+  const [todo, onChangeTodo] = React.useState('');
+
+  const addTodo = async () => {
+    try {
+      const response = await fetch('https://shadowy-shadow-4wq45j95wx639wp-3000.app.github.dev/agrega_todo', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ todo }),
+      });
+
+      if (response.status === 201) {
+        const data = await response.json();
+        console.log('Todo added:', data);
+        Alert.alert('Éxito', '¡Todo agregado exitosamente!');
+      } else {
+        console.error('Error adding todo:', response.statusText);
+        Alert.alert('Error', 'Hubo un problema al agregar el todo.');
+      }
+    } catch (error) {
+      console.error('Error adding todo:', error);
+      Alert.alert('Error', 'Hubo un problema al agregar el todo.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        placeholder="todo"
+        textContentType="default"
+        keyboardType="default"
+        onChangeText={(text) => onChangeTodo(text)}
+      />
+
+      <Button
+        style={styles.button}
+        title="Agregar todo"
+        onPress={() => addTodo()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginLeft: 20,
+    marginEnd: 20,
+  },
+  textInput: {
+    height: 40,
+    padding: 8,
+    marginBottom: 20,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  button: {},
+});
+
+export default App;
